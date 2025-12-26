b765f23b271e6a8ed29eb80cea80cd7d8ad4cb56
Gaston Coria <gaston.coria@uala.com.ar>
2025-12-26 17:47:50 -0300

    add declare channel function in pubsub

--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -7,6 +7,13 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+type SimpleQueueType string
+
+const (
+	SimpleQueueTypeDurable   SimpleQueueType = "durable"
+	SimpleQueueTypeTransient SimpleQueueType = "transient"
+)
+
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	json, err := json.Marshal(val)
 	if err != nil {
@@ -20,4 +27,42 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 		},
 	)
 	return nil
+}
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType,
+) (*amqp.Channel, amqp.Queue, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	queue, err := ch.QueueDeclare(
+		queueName,                             // name
+		queueType == SimpleQueueTypeDurable,   // durable
+		queueType == SimpleQueueTypeTransient, // delete when unused
+		queueType == SimpleQueueTypeTransient, // exclusive
+		false,                                 // no-wait
+		nil,                                   // arguments
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	err = ch.QueueBind(
+		queue.Name, // queue name
+		key,        // routing key
+		exchange,   // exchange
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	return ch, queue, nil
 }
